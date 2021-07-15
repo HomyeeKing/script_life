45ee2527f28d743654fdbd86c81f9656167fff3e
Homyee King <HomyeeKing@gmail.com>
2021-07-15 21:36:02 +0800

    fix: multi remote

--- packages/switch-helper/test.sh
+++ packages/switch-helper/test.sh
@@ -1,2 +1,14 @@
-name=$1
-[[ $name == 'homy' ]] && echo $name || echo 'tom'
+# unary expression
+# name=$1
+# [[ $name == 'homy' ]] && echo $name || echo 'tom'
+
+# str split 
+
+function str2arr(){
+    return ( `echo $1 | tr ',' ' '` )
+}
+str="origin upstream"
+arr=str2arr(str) # conver str ---> array
+echo ${arr}
+# name=$([[ $(git remote | wc -l) -ge 1 ]] && ${str//,/ })
+# echo $name
